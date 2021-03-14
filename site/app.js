e8e1cca89f34c784c0fbd3100fac728401605dc5
Eamonn O'Brien-Strain <eob@well.com>
2021-03-13 19:33:20 -0800

    Unstyled gallery.

--- site/app.js
+++ site/app.js
@@ -1,3 +1,5 @@
+/* global galleryFigureElement, galleryImageElement, prevElement, exitElement, nextElement */
+
 // Credit: Dan https://stackoverflow.com/a/7557433/978525
 const isElementInViewport = el => {
   const rect = el.getBoundingClientRect()
@@ -31,7 +33,46 @@ const showVisible = () => {
   })
 }
 
+const showGallery = (hrefs, i) => {
+  galleryFigureElement.classList.add('shown')
+  galleryImageElement.setAttribute('src', hrefs[i])
+  const n = hrefs.length
+  prevElement.onclick = () => {
+    const iPrev = (i + n - 1) % n
+    window.location = `#fig-${iPrev}`
+    showGallery(hrefs, iPrev)
+  }
+  nextElement.onclick = () => {
+    const iNext = (i + 1) % n
+    window.location = `#fig-${iNext}`
+    showGallery(hrefs, iNext)
+  }
+  exitElement.onclick = () => {
+    galleryFigureElement.classList.remove('shown')
+  }
+}
+
 document.body.onload = () => {
   showVisible()
   document.body.onscroll = showVisible
+
+  const mediumImages = document.getElementsByClassName('medium-image')
+  const n = mediumImages.length
+  const hrefs = []
+  for (let i = 0; i < n; ++i) {
+    const aElement = mediumImages[i]
+    hrefs.push(aElement.getAttribute('href'))
+    aElement.setAttribute('href', '#')
+    mediumImages[i].onclick = () => {
+      window.location = `#fig-${i}`
+      showGallery(hrefs, i)
+    }
+  }
 }
+
+/* window.onpopstate = event => {
+  if (!event.state) {
+    return
+  }
+  showGallery(event.state.hrefs, event.state.i)
+} */
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import {
   a,
   article,
   body,
+  button,
+  figure,
+  figcaption,
   h1,
   h2,
   head,
@@ -34,6 +37,7 @@ console.log('<!doctype html>',
       title('Exploring the Mandelbrot Set'),
       link({ rel: 'stylesheet', href: 'tufte-css-gh-pages/tufte.min.css' }),
       link({ rel: 'stylesheet', href: 'tufte-css-gh-pages/latex.css' }),
+      link({ rel: 'stylesheet', href: 'gallery.css' }),
       meta({ name: 'viewport', content: 'width=device-width, initial-scale=1' }),
       link({ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }),
       link({ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }),
@@ -275,6 +279,14 @@ console.log('<!doctype html>',
               'Modifying the almondbread software to use 80-bit double-precision values',
               'allows zooming to this unsusal short linear feature at',
               '1,000,000,000,000,000× (one quadrillion) magnification.')
+          ),
+          section(
+            figure({ id: 'galleryFigureElement' },
+              button({ id: 'prevElement' }, 'Previous'),
+              button({ id: 'exitElement' }, 'Exit'),
+              button({ id: 'nextElement' }, 'Next'),
+              img({ id: 'galleryImageElement' }),
+              figcaption({ id: 'galleryCaptionElement' }))
           ),
           section(
             p({
