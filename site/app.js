9785ca89644e8074860d5c04ca25fdb73098faeb
Eamonn O'Brien-Strain <eob@well.com>
2021-03-13 22:24:31 -0800

    Better zooming.

--- site/app.js
+++ site/app.js
@@ -44,19 +44,19 @@ const setGalleryImageSize = (width, height) => {
 }
 
 let zoomed = false
-galleryImageElement.onclick = ({ clientX, clientY }) => {
+galleryImageElement.onclick = ({ offsetX, offsetY }) => {
   const classes = galleryImageElement.classList
+  const imageHeight = window.screen.width * HD_HEIGHT_PX / HD_WIDTH_PX
   if (zoomed) {
     classes.remove('zoom')
-    setGalleryImageSize(window.screen.width,
-      window.screen.width * HD_HEIGHT_PX / HD_WIDTH_PX)
+    setGalleryImageSize(window.screen.width, imageHeight)
     galleryImageElement.style.left = 0
-    galleryImageElement.style.top = 0
+    galleryImageElement.style.top = px((window.screen.height - imageHeight) / 2)
   } else {
     classes.add('zoom')
     setGalleryImageSize(HD_WIDTH_PX, HD_HEIGHT_PX)
-    galleryImageElement.style.left = px(window.screen.width / 2 - HD_WIDTH_PX * clientX / window.screen.width)
-    galleryImageElement.style.top = px(window.screen.height / 2 - HD_WIDTH_PX * clientY / window.screen.width)
+    galleryImageElement.style.left = px(window.screen.width / 2 - HD_WIDTH_PX * offsetX / window.screen.width)
+    galleryImageElement.style.top = px(window.screen.height / 2 - HD_HEIGHT_PX * offsetY / imageHeight)
   }
   zoomed = !zoomed
 }
@@ -69,17 +69,19 @@ const showGallery = (hrefs, i) => {
     document.body.classList.remove('gallery-shown')
   }
   galleryImageElement.setAttribute('src', hrefs[i])
-  setGalleryImageSize(window.screen.width,
-    window.screen.width * HD_HEIGHT_PX / HD_WIDTH_PX)
+  const imageHeight = window.screen.width * HD_HEIGHT_PX / HD_WIDTH_PX
+  setGalleryImageSize(window.screen.width, imageHeight)
+  galleryImageElement.style.left = 0
+  galleryImageElement.style.top = px((window.screen.height - imageHeight) / 2)
   const n = hrefs.length
   prevElement.onclick = () => {
     const iPrev = (i + n - 1) % n
-    window.location = `#fig-${iPrev}`
+    // window.location = `#fig-${iPrev}`
     showGallery(hrefs, iPrev)
   }
   nextElement.onclick = () => {
     const iNext = (i + 1) % n
-    window.location = `#fig-${iNext}`
+    // window.location = `#fig-${iNext}`
     showGallery(hrefs, iNext)
   }
 }
