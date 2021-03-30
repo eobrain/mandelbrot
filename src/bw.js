6aefb825c3b035a11d4ded0909f079ac6f195fa8
Eamonn O'Brien-Strain <eob@well.com>
2021-03-29 21:19:34 -0700

    Fix scale text.

--- src/bw.js
+++ src/bw.js
@@ -24,12 +24,12 @@ console.log(page(section(
     '-1.9532566366044803_0.0000013417179314264052_1.1641532182693481e-10_100000_bw',
     '-1.953256636604 +i0.000001341718',
     'At 8,000,000× magnification.',
-    'If the Mandelbot set were the size of the Earth this image would be 66 cm (26 inches) wide.'),
+    'If the Mandelbot set were the size of the Earth this image would be 0.6 millimetres wide.'),
   mandelbrot(
     '0.3508966269802502_-0.42142952345047896_5e-13_10000000_bw',
     '0.350896626980250 -i0.421429523450479',
     'At 9,000,000,000× (nine billion) magnification.',
-    'If the Mandelbot set were the size of the Earth this image would be 0.6 millimetres wide.'),
+    'If the Mandelbot set were the size of the Earth this image would be 3 μm wide.'),
   mandelbrot(
     '-0.10375257638744859_0.9250308323744141_2.842170943040401e-14_100000_bw',
     '-0.103752576387449 +i0.9250308323744141',
